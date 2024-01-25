b75901106d22ff6be051c1be797c99ae8230583a
HalynaLysun <125215419+HalynaLysun@users.noreply.github.com>
2024-01-25 22:04:12 +0200

    task-3 and task-4

--- js/task-3.js
+++ js/task-3.js
@@ -1,1 +1,15 @@
 'use strict'
+
+const userName = document.querySelector("input")
+const helloUser = document.querySelector("#name-output")
+
+
+function inputHandler(event) {
+    helloUser.textContent = userName.value.trim() || 'Anonymous'
+}
+
+
+
+userName.addEventListener('input', inputHandler)
+
+    
--- js/task-4.js
+++ js/task-4.js
@@ -1,2 +1,20 @@
 
 'use strict'
+
+const form = document.querySelector('.login-form')
+
+function formHandler(event) {
+    event.preventDefault()
+    if (event.target.email.value === "" || event.target.password.value === "") {
+       alert('All form fields must be filled in')
+    } else {
+        const data = {
+        email: event.target.email.value.trim(),
+        password: event.target.password.value.trim()
+    }
+        console.log(data)
+        form.reset()
+    }
+}
+
+form.addEventListener('submit', formHandler)
