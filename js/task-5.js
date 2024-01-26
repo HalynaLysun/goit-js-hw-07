d8523453ef3af15c643afa74a7e6f008e0e69d0a
HalynaLysun <125215419+HalynaLysun@users.noreply.github.com>
2024-01-26 07:29:21 +0200

    task-5

--- js/task-5.js
+++ js/task-5.js
@@ -1,7 +1,18 @@
 'use strict'
 
+const bodyEl = document.querySelector('body')
+const pressEl = document.querySelector('button')
+const spanEl = document.querySelector('span')
+
+function clickHandler (event) {
+  bodyEl.style.backgroundColor = getRandomHexColor()
+  spanEl.textContent = getRandomHexColor()
+}
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+pressEl.addEventListener('click', clickHandler)
